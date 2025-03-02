b293bd97c140277a3924f3012f75cf7e723a02ae
Xiru Chen <xiruchen1024@gmail.com>
2025-03-02 22:31:57 +0200

    comment out InsightItemByUserItem resource
    to be discussed

--- geodata/api.py
+++ geodata/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restx import Api
 from .resources.user import UserCollection, UserItem
-from .resources.insight import InsightCollectionByUserItem, InsightItemByUserItem, AllInsights
+from .resources.insight import InsightCollectionByUserItem, AllInsights
 from .resources.feedback import (
     FeedbackCollectionByUserInsightItem,
       FeedbackCollectionByUserItem,
@@ -17,7 +17,7 @@ api.add_resource(UserItem, "/users/<user:user>/")
 
 api.add_resource(InsightCollectionByUserItem, "/users/<user:user>/insights/")
 api.add_resource(FeedbackCollectionByUserItem, "/users/<user:user>/feedbacks/")
-api.add_resource(InsightItemByUserItem, "/users/<user:user>/insights/<insight:insight>/")
+# api.add_resource(InsightItemByUserItem, "/users/<user:user>/insights/<insight:insight>/")
 api.add_resource(
     FeedbackCollectionByUserInsightItem,
       "/users/<user:user>/insights/<insight:insight>/feedbacks/")
