718566d460eb6b0d250cc9305e96c6cad07f31fe
MagusDev <m.agame1379@gmail.com>
2025-03-02 20:04:00 +0200

    api linting fix

--- geodata/api.py
+++ geodata/api.py
@@ -2,7 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 from .resources.user import UserCollection, UserItem
 from .resources.insight import InsightCollectionByUserItem, InsightItemByUserItem, AllInsights
-from .resources.feedback import FeedbackCollectionByUserInsightItem, FeedbackCollectionByUserItem, FeedbackItemByUserInsightItem
+from .resources.feedback import (
+    FeedbackCollectionByUserInsightItem,
+      FeedbackCollectionByUserItem,
+        FeedbackItemByUserInsightItem
+)
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
@@ -14,6 +18,10 @@ api.add_resource(UserItem, "/users/<user:user>/")
 api.add_resource(InsightCollectionByUserItem, "/users/<user:user>/insights/")
 api.add_resource(FeedbackCollectionByUserItem, "/users/<user:user>/feedbacks/")
 api.add_resource(InsightItemByUserItem, "/users/<user:user>/insights/<insight:insight>/")
-api.add_resource(FeedbackCollectionByUserInsightItem, "/users/<user:user>/insights/<insight:insight>/feedbacks/")
-api.add_resource(FeedbackItemByUserInsightItem, "/users/<user:user>/insights/<insight:insight>/feedbacks/<feedback:feedback>/")
+api.add_resource(
+    FeedbackCollectionByUserInsightItem,
+      "/users/<user:user>/insights/<insight:insight>/feedbacks/")
+api.add_resource(
+    FeedbackItemByUserInsightItem,
+      "/users/<user:user>/insights/<insight:insight>/feedbacks/<feedback:feedback>/")
 api.add_resource(AllInsights, "/insights/")
