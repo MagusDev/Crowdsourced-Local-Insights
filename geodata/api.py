6678b4f5a43a4b916cd93961aaad984fba92da05
Xiru Chen <xiruchen1024@gmail.com>
2025-03-23 13:06:36 +0200

    decouple - add api init file
    decouple and avoid circular import

--- geodata/api.py
+++ geodata/api.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from flask_restx import Api
+
+from .api_init import api
 from .resources.user import UserCollection, UserItem
 from .resources.insight import InsightCollectionByUserItem, AllInsights, InsightItemByUserItem
 from .resources.feedback import (
@@ -8,21 +8,16 @@ from .resources.feedback import (
         FeedbackItemByUserInsightItem
 )
 
-api_bp = Blueprint("api", __name__, url_prefix="/api")
-# GPT used to set swagger
-api = Api(api_bp, doc="/swagger")
-
 api.add_resource(UserCollection, "/users/")
 api.add_resource(UserItem, "/users/<user:user>/")
 
-api.add_resource(InsightCollectionByUserItem, "/users/<user:user>/insights/")
 api.add_resource(FeedbackCollectionByUserItem, "/users/<user:user>/feedbacks/")
-api.add_resource(InsightItemByUserItem, "/users/<user:user>/insights/<insight:insight>/")
-
+api.add_resource(InsightCollectionByUserItem, "/users/<user:user>/insights/")
+api.add_resource(InsightItemByUserItem, "/users/<user:user>/insight/")
+api.add_resource(AllInsights, "/insights/")
 api.add_resource(
     FeedbackCollectionByUserInsightItem,
       "/users/<user:user>/insights/<insight:insight>/feedbacks/")
 api.add_resource(
     FeedbackItemByUserInsightItem,
       "/users/<user:user>/insights/<insight:insight>/feedbacks/<feedback:feedback>/")
-api.add_resource(AllInsights, "/insights/")
