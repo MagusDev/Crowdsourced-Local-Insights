44513812f4cd78599c6d01ba0e6b0e26641c705b
MagusDev <m.agame1379@gmail.com>
2025-03-26 17:03:21 +0200

    auth.py linting improvement

--- geodata/auth.py
+++ geodata/auth.py
@@ -4,16 +4,18 @@ Authentication module for the API.
 
 import secrets
 from flask import request
-
 from werkzeug.exceptions import Forbidden
 from .models import ApiKey
 
 def require_admin(func):
+    """
+    Decorator to require an admin API key.
+    """
     def wrapper(*args, **kwargs):
         api_key = request.headers.get("Geodata-Api-Key")
         if not api_key:
             raise Forbidden("API key required")
-            
+
         key_hash = ApiKey.key_hash(api_key.strip())
         db_key = ApiKey.query.filter_by(admin=True).first()
         if db_key and secrets.compare_digest(key_hash, db_key.key):
@@ -22,14 +24,17 @@ def require_admin(func):
     return wrapper
 
 def require_user_auth(func):
+    """
+    Decorator to require a user API key.
+    Mainly used for user specific endpoints put and delete methods.
+    """
     def wrapper(*args, **kwargs):
-        self = args[0]  # Get the resource instance
         user = kwargs.get('user')  # Get the user from kwargs
-        
+
         api_key = request.headers.get("Geodata-Api-Key")
         if not api_key:
             raise Forbidden("API key required")
-            
+
         key_hash = ApiKey.key_hash(api_key.strip())
         db_key = ApiKey.query.filter_by(user_id=user.id).first()
         if db_key and secrets.compare_digest(key_hash, db_key.key):
