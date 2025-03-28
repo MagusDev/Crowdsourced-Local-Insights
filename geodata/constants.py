3092911fefece5b41220b8ba2d7ee51c3e5e2028
Touko Kinnunen <toukosovellukset@gmail.com>
2025-03-28 13:23:12 +0200

    constants added for mason profile and rel

--- geodata/constants.py
+++ geodata/constants.py
@@ -2,3 +2,8 @@
 This module includes app constants.
 """
 MASON = "application/vnd.mason+json"
+
+USER_PROFILE_URL = "/profiles/user/"
+INSIGHT_PROFILE_URL = "/profiles/insight/"
+FEEDBACK_PROFILE_URL = "/profiles/feedback/"
+LINK_RELATIONS_URL = "/geometa/link-relations#"
