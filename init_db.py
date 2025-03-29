5eaf97647b4e92ef5dca5bca255779b91a598149
Xiru Chen <xiruchen1024@gmail.com>
2025-03-29 19:20:39 +0200

    Add init_db

---
+++ init_db.py
@@ -0,0 +1,10 @@
+"""
+This module initializes db for app.
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from geodata import db
+from run import app
+
+with app.app_context():
+    db.create_all()
