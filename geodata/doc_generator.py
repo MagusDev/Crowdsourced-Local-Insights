47baff94572ea9faae78417086d2f703f1858a9d
Xiru Chen <xiruchen1024@gmail.com>
2025-04-10 18:43:51 +0300

    update

--- geodata/doc_generator.py
+++ geodata/doc_generator.py
@@ -15,8 +15,8 @@ DOC_TEMPLATE = {
         }
     }
 }
-endPointName = "insights"
-resp_json = requests.get(SERVER_ADDR + "/insights/?usr=test3").json()
+endPointName = "insights_by"
+resp_json = requests.get(SERVER_ADDR + "/users/test3/insights/").json()
 DOC_TEMPLATE["responses"]["200"]["content"]["application/vnd.mason+json"]["example"] = resp_json
 with open(os.path.join(DOC_ROOT, f"{endPointName}/get.yml"), "w") as target:
     target.write(yaml.dump(DOC_TEMPLATE, default_flow_style=False))
